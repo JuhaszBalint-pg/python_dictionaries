987b4068163ea5d182dbe0809851c829f3795635
JuhaszBalint-pg <juhasz.balint@diak.szbi-pg.hu>
2025-12-10 11:31:19 +0100

    datatypes

--- feladatok/3_2.py
+++ feladatok/3_2.py
@@ -11,14 +11,48 @@ kedvencjatek = {
 
 for kulcs, ertek in kedvencjatek.items():
     print(f'{kulcs} : {ertek}')
+
 #függvény helye
+def errors(x):
+    # try:
+    #     if '.' in x:
+    #         result = float(x)
+    #         return result
+    #     else:
+    #         result = int(x)
+    #         return result
+    # except ValueError as e:
+    #     if  x == 'True' or x == 'true' or x == 'igaz' or x == 'Igaz':
+    #          result = True
+    #          return result
+    #     elif x == 'False' or x == 'false' or x == 'Hamis' or x == 'hamis':
+    #          result = False
+    #          return result
+
+
+
+    
+    try:
+        if x.isnumeric():
+            return int(x)
+        elif x.count('.') > 0 and x.count('.') < 0:
+            return float(x)
+        elif x == 'True' or x == 'true' or x == 'igaz' or x == 'Igaz':
+             return True
+        elif x == 'False' or x == 'false' or x == 'Hamis' or x == 'hamis':
+            return  False
+        else:
+            return str(x)
+    except ValueError as e:
+        print(f'{e}, a program value errorba futott.')
+        
 cycle = input('szeretnéd bővíteni az adatszerkezetet? (i/n)')
 if cycle == 'i':
     fut = True
     while fut:
         adatkulcsneve = input('Add meg az adat nevét!\n')
-        kedvencjatek[adatkulcsneve] = input('Add meg az adatszerkezet értékét!\n')
-        #value error, függvény hívása
+        adat_ertek = input('Add meg az adatszerkezet értékét!\n')
+        print(type(errors(adat_ertek)))
         question = input('Szeretnél további adatokat hozzáadni? (i/n)')
         if question == 'i':
             continue
